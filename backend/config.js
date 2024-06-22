ab62b762b0cb6940bc6dba3261dfe1aea1179a3e
binzam <btechan@gmail.com>
2024-06-23 01:36:53 +0300

    user model & ordermodel

--- backend/config.js
+++
@@ -1,4 +0,0 @@
-export const PORT = 5555;
-
-export const mongoDbURL =
-  'mongodb+srv://admin-bini:fMODUirMEehYh2XS@backenddb.le8n5h6.mongodb.net/bt-shop-db?retryWrites=true&w=majority&appName=BackendDB';
