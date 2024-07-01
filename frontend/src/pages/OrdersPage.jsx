3f95250ec6b227dde0cdd12acbbfdc0d59428b49
binzam <btechan@gmail.com>
2024-07-01 04:31:24 +0300

    create order requers

--- frontend/src/pages/OrdersPage.jsx
+++ frontend/src/pages/OrdersPage.jsx
@@ -1,8 +1,53 @@
+import axios from 'axios';
+import { useAuthContext } from '../hooks/useAuthContext';
 
 const OrdersPage = () => {
-  return (
-    <div>OrdersPage</div>
-  )
-}
+  const { user } = useAuthContext();
+  const createOrder = async (orderData) => {
+    try {
+      const response = await axios.post(
+        'http://localhost:5555/orders',
+        orderData,
+        {
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        }
+      );
+      return response.data;
+    } catch (error) {
+      console.error('Error creating order:', error);
+      throw new Error('Failed to create the order. Please try again.');
+    }
+  };
 
-export default OrdersPage
+  const newOrder = {
+    user: user._id,
+    orders: [
+      {
+        product: 'product_id',
+        quantity: 4,
+        price: .99,
+      },
+    ],
+    totalAmount: 51.98,
+    shippingAddress: {
+      street: '',
+      city: '',
+      country: '',
+    },
+    status: 'Pending',
+  };
+
+  createOrder(newOrder)
+    .then((createdOrder) => {
+      console.log('Order created successfully:', createdOrder);
+    })
+    .catch((error) => {
+      console.error('Error creating order:', error.message);
+    });
+
+  return <div>OrdersPage</div>;
+};
+
+export default OrdersPage;
